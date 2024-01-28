6c731693dcb60260108ab961cf1a6d0b36e6b82e
ThomasTenDamme <ttdamme@gmail.com>
2024-01-28 14:37:48 +0100

    Added NaSch model (run_model_stochastic)

--- modules.py
+++ modules.py
@@ -1,6 +1,8 @@
+import random, concurrent.futures, scipy.optimize
 import numpy as np
-import random
 import cellpylib as cpl
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 from collections import Counter
 
 class CA:
@@ -128,11 +130,18 @@ class CA:
         return self.initial_state
 
 
-def find_elem_jams(evolution):
+def find_jams(evolution, add_lifespans = False):
     """
     A function to find the jams in the evolution of a CA. It checks every row for groupings of 1's that are bigger than 1.
     Then it checks if it can find the same group in the next row, if so, it adds the length of the group to the size of the jam.
     The resulting list contains the starting and ending index of the last row that contained the jams, as well as the size of the jam.
+
+    Parameters:
+    - evolution (list): The evolution of the CA.
+    - add_lifespans (bool): Whether or not to add the lifespans of the jams to the result.
+
+    Returns:
+    - result (list): A list containing the starting and ending index of the last row that contained the jams, as well as the size of the jam. If add_lifespans is True, it also contains the lifespans of the jams.
     """
 
     def row_jams(row):
@@ -155,6 +164,8 @@ def find_elem_jams(evolution):
         return result
 
     previous_jams = [[x, x[1] - x[0] + 1] for x in row_jams(evolution[0])]
+    lifespans = [1] * len(previous_jams)
+
     for row in evolution[1:]:
         current_jams = row_jams(row)
 
@@ -167,8 +178,12 @@ def find_elem_jams(evolution):
                 if current_jam[1] == right_cell - 1:
                     previous_jams[i][1] += current_jam[1] - current_jam[0] + 1
                     previous_jams[i][0] = current_jam
+                    lifespans[i] += 1
                     break
     
+    if add_lifespans:
+        return previous_jams, lifespans
+
     return previous_jams
 
 def initial_to_random_walk(initial_state):
@@ -224,7 +239,7 @@ def triangulize_evolution(evolution):
 
 def run_model(p, L, T, n_repetitions = 100):
     """
-    Function to run the model for a given p, L and T. It returns the lifespans and jam sizes of all the jams found in the evolution of the CA.
+    Function to run the CA rule 184 model for a given p, L and T. It returns the lifespans and jam sizes of all the jams found in the evolution of the CA.
     
     Parameters:
     - p (float): The probability of a cell being 1 in the initial state.
@@ -240,7 +255,7 @@ def run_model(p, L, T, n_repetitions = 100):
     total_lifespans = []
     total_jam_sizes = []
 
-    for i in range(n_repetitions):
+    for _ in range(n_repetitions):
         initial_state = ca_184.gen_initial_state_bernoulli(L, p)
         random_walk = initial_to_random_walk(initial_state)
         cellular_automaton = cpl.evolve(initial_state, timesteps=T, memoize=True, apply_rule=lambda n, c, t: cpl.nks_rule(n, rule=184))
@@ -249,12 +264,257 @@ def run_model(p, L, T, n_repetitions = 100):
         lifespans = jam_lifespans(random_walk)
         total_lifespans += lifespans
         
-        jams = find_elem_jams(cellular_automaton)
+        jams = find_jams(cellular_automaton)
         jam_sizes = [jam[1] for jam in jams]
         total_jam_sizes += jam_sizes
     
     lifespan_counter = Counter(total_lifespans)
     jam_counter = Counter(total_jam_sizes)
     return lifespan_counter, jam_counter
+
+def initial_state_nasch(L, p, v_max):
+    """
+    Function to generate an initial state for the NaSch model. It returns the initial state of the CA.
+
+    Parameters:
+    - L (int): The length of the CA.
+    - p (float): The probability of a cell being 1 in the initial state.
+    - v_max (int): Maximum speed of vehicles.
+
+    Returns:
+    - initial_state (list): The initial state of the CA. It contains a tuple for every cell, with the first element indicating whether the cell is occupied and the second element indicating the speed of the vehicle.
+    """
+    initial_state = [(False, 0) if random.random() > p else (True, random.randint(1, v_max)) for _ in range(L)]
+    return initial_state
+
+def nasch_step(current_state, v_max, p_slowdown):
+    """
+    Function to perform a single timestep of the NaSch model. It returns the next state of the CA.
+
+    Parameters:
+    - current_state (list): The current state of the CA. It contains a tuple for every cell, with the first element indicating whether the cell is occupied and the second element indicating the speed of the vehicle.
+    - v_max (int): Maximum speed of vehicles.
+    - p_slowdown (float): Probability of slowing down.
+
+    Returns:
+    - next_state (list): The next state of the CA. Same format as current_state.
+    """
+
+    # Acceleration: Increase the speed of each vehicle by 1, up to the maximum speed
+    current_state = [(x[0], min(x[1] + 1, v_max)) for x in current_state]
+
+    # Slowing down: Check the distance to the next vehicle, and reduce the speed to that distance if it is smaller than the current speed
+    for i, (car_present, velocity) in enumerate(current_state):
+        if not car_present:
+            continue
+
+        distance_to_next_car = 0
+        for j in range(i + 1, len(current_state) + 1):
+            # If we reach the end of the road, we set the distance high so the car will leave the simulation
+            if j == len(current_state):
+                distance_to_next_car = len(current_state)
+                break
+            
+            # If we find a car, we stop counting
+            if current_state[j][0]:
+                break
+
+            distance_to_next_car += 1
+        
+        # Reduce the speed to the distance to the next car if it is smaller than the current speed
+        current_state[i] = (current_state[i][0], min(distance_to_next_car, current_state[i][1]))
     
+    # Randomization: Reduce the speed of each vehicle by 1 with probability p_slowdown
+    current_state = [(x[0], max(x[1] - 1, 0) if random.random() < p_slowdown else x[1]) for x in current_state]
+
+    # Movement: Move each vehicle forward by its speed
+    next_state = [(False, 0)] * len(current_state)
+    for i, (car_present, velocity) in enumerate(current_state):
+        if not car_present or i + velocity >= len(current_state):
+            continue
+        
+        next_state[i + velocity] = (True, velocity)
+
+    return next_state
+            
+
+#changed run model function introducing stochasticity/dynamics Influx=Outflux 
+def run_model_stochastic(p, L, T, n_repetitions=100, v_max=5, p_slowdown=0.1, triangular=False, visualize=False):
+    """
+    Function to run the NaSch model for a given p, L, T. It returns the lifespans and jam sizes of all the jams found in the evolution of the model.
+
+    Parameters:
+    - p (float): The probability of a cell being 1 in the initial state.
+    - L (int): The length of the simulation.
+    - T (int): The number of timesteps.
+    - n_repetitions (int): The number of times the model should be run.
+    - v_max (int): Maximum speed of vehicles.
+    - p_slowdown (float): Probability of slowing down.
+
+    Returns:
+    - lifespan_counter (Counter): A counter with the lifespans of all the jams found in the evolutions of the model.
+    - jam_counter (Counter): A counter with the sizes of all the jams found in the evolutions of the model.
+    """
+    total_lifespans = []
+    total_jam_sizes = []
+
+    for i in range(n_repetitions):
+        # Create initial state
+        initial_state = initial_state_nasch(L, p, v_max)
+
+        # Run the model
+        evolution = [initial_state]
+        for t in range(T):
+            evolution.append(nasch_step(evolution[-1], v_max, p_slowdown))
+        
+        location_states = np.array([[cell[0] for cell in state] for state in evolution])
+
+        if triangular:
+            location_states = triangulize_evolution(location_states)
+
+        jams, lifespans = find_jams(location_states, add_lifespans=True)
+        
+        jam_sizes = [jam[1] for jam in jams]
+        total_jam_sizes += jam_sizes
+        total_lifespans += lifespans
+
+    lifespan_counter = Counter(total_lifespans)
+    jam_counter = Counter(total_jam_sizes)
+    return lifespan_counter, jam_counter
+
+def visualize_jam_counter(jam_counter, fit_line = False):
+    plt.figure(figsize=(12,6))
+    plt.title(f'Jam Sizes')
+    plt.loglog(range(1, int(max(jam_counter.keys()))), [jam_counter[i] for i in range(1, int(max(jam_counter.keys())))], 'o')
+    plt.xlabel('Jam size')
+    plt.ylabel('Number of jams')
+
+    if fit_line:
+        lists = sorted(jam_counter.items())
+        x, y = zip(*lists)
+        x_log, y_log = np.log(x), np.log(y)
+
+        # Fit linear function to log-log data   
+        def func(x, a, b):
+            return -a * x + b
+        
+        def power_law_func(x, a, b):
+            return a * x ** -b
+
+        popt, _ = scipy.optimize.curve_fit(func, x_log, y_log, p0=[1, 0.5])
+        a_optimal, b_optimal = popt
+
+        # plot the fitted line
+        plt.plot(x, power_law_func(x, np.exp(b_optimal), a_optimal), label=f'Linear fit on log-log data: y = {np.exp(b_optimal):.2f}x^-{a_optimal:.2f}')
+
+        # Fit power-law function
+        popt, _ = scipy.optimize.curve_fit(power_law_func, x, y, p0=[1, 0.5])
+        a_optimal, b_optimal = popt
+
+        # plot the fitted line
+        plt.plot(x, power_law_func(x, a_optimal, b_optimal), label=f'Power-law fit: y = {a_optimal:.2f}x^-{b_optimal:.2f}')
+
+    plt.legend()
+    plt.show()
+
+def run_model_wrapper(args):
+    # Function to run the model with a single argument, so it can be used with concurrent.futures.ProcessPoolExecutor's map function.
+    return run_model(args[0], args[1], args[2], args[3])
+
+def run_model_for_densities(p_values, L, T, p_repetitions, n_repetitions, concurrently = True):
+    """
+    Function to run the model for a given list of p values, L and T. It fits a power law to the data and returns p_repetitions fitted exponents for every p value.
+
+    Parameters:
+    - p_values (list): The list of p values to run the model for.
+    - L (int): The length of the CA.
+    - T (int): The number of timesteps.
+    - p_repetitions (int): The number of times the model should be run for each p value.
+    - n_repetitions (int): The number of times the model should be run to estimate the exponent once.
+    - concurrently (bool): Whether or not to run the model concurrently.
+
+    Returns:
+    TODO
+    """
+
+    # with concurrent.futures.ProcessPoolExecutor() as executor:
+    #     results = tqdm_parallel_map(executor, assigned_material_job, job_ids)
+
+    results_dict = {}
+
+    for p in p_values:
+        print(f"Running model for p = {p}")
+        results_dict[p] = []
+
+        args = [(p, L, T, n_repetitions)] * p_repetitions
+
+        if not concurrently:
+            raise NotImplementedError("Consecutive version not implemented yet")
+
+        if concurrently:
+            with concurrent.futures.ProcessPoolExecutor() as executor:
+                output = list(tqdm(executor.map(run_model_wrapper, args), total=len(args)))
+
+                # Now for every output, we have two counters. From the jam size counter we want to fit an exponent
+                for out in output:
+                    jam_counter = out[1]
+                    
+                    
+
+                    lists = sorted(jam_counter.items())
+                    x, y = zip(*lists)
+                    x_log, y_log = np.log(x), np.log(y)
+
+                    # Fit linear function to log-log data   
+                    def func(x, a, b):
+                        return -a * x + b
+                    
+                    test_index = int(len(x_log) * 0.8)
+
+                    popt, _ = scipy.optimize.curve_fit(func, x_log[:test_index], y_log[:test_index], p0=[1, 0.5])
+                    a_optimal, _ = popt
+
+                    results_dict[p].append(a_optimal)
+
+                    # visualize_jam_counter(jam_counter, fit_line = True)
+
+                    # visualize_jam_counter(jam_counter, fit_line = False)
+
+                    # lists = sorted(jam_counter.items())
+                    # x, y = zip(*lists)
+
+                    # # Fit power-law function
+                    # def func(x, a, b):
+                    #     return a * x ** -b
+
+                    # popt, _ = scipy.optimize.curve_fit(func, x, y, p0=[1, 0.5])
+                    # _, b_optimal = popt
+
+                    # results_dict[p].append(b_optimal)
+        
+        print(f"Results for p = {p}: {results_dict[p]}")
+    
+    # Visualize exponents for every p value
+    plt.figure(figsize=(12,6))
+    plt.title(f'Exponent for every p value')
+    plt.plot(p_values, results_dict.values(), 'o')
+    plt.xlabel('p')
+    plt.ylabel('Exponent')
+
+    # # Fit line through p_values and exponents 
+    # def func(x, a, b):
+    #     return a * x + b
+    
+    # popt, _ = scipy.optimize.curve_fit(func, x, y, p0=[1, 0.5])
+    # a_optimal, b_optimal = popt
+
+    # plt.plot(x, func(x, a_optimal, b_optimal), label=f'Linear fit: y = {a_optimal:.2f}x + {b_optimal:.2f}')
+    
+    plt.show()
+
+    return results_dict
+        
+
+        
+
 
